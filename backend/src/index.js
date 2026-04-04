ad35de96616d62738e810ec062d65dc5cd04176b
Soham-Codes17 <soham.n7805@gmail.com>
2026-04-04 20:11:38 +0530

    ready for deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,9 @@
 import express from "express"
+import path from "path"
 import authRoutes from "./routes/auth.route.js";
 import ENV from "./lib/env.js"
 import { connectDB } from "./lib/db.js";
-import path from "path";
+// import path from "path";
 import cookieParser from "cookie-parser"
 import messageRoutes from "./routes/message.route.js"
 
@@ -10,6 +11,8 @@ import messageRoutes from "./routes/message.route.js"
 
 
 const app = express();
+const __dirname  = path.resolve();
+
 const PORT = ENV.PORT || 3000 ;
 
  
@@ -19,6 +22,16 @@ app.use(cookieParser())
 
 app.use("/api/auth" , authRoutes  )
 app.use("/api/messages", messageRoutes)
+
+
+// make ready for the deployment 
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get(/.*/, (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
   
 const startServer = async () => {
   try {
